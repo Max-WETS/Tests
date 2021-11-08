87a517a7bcc2101896bee105f551dfc65bc9586c
Maxime Wets <wetsmaxime@gmail.com>
2021-11-08 12:06:03 +0100

    cleaned document

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const jsonfile = require("jsonfile");
 const configurationFile = require("./configurationFile.json");
 const changesList = require("./changesList3.json");
 
@@ -22,50 +21,36 @@ const pathController = (config, changeArr, level) => {
   const instruction = (() => {
     let obj = config;
     slicedArr = changeArr.slice(0, level);
-    console.log("slice array: ", slicedArr);
-    console.log("inputs array length: ", changeArr.length);
     return slicedArr.reduce((prev, curr) => {
       return prev[curr];
     }, obj);
   })();
 
-  console.log(instruction);
-  //   console.log(config["page1"]);
-
   if (instruction) {
     if (level < changeArr.length) {
       if (typeof instruction === "object") {
         level++;
-        console.log("level", level);
         validPath = pathController(config, changeArr, level);
       } else {
         errorMessage =
           "The declared transformation path doesn't exist in the config file";
-        console.log("this path doesn't exist");
       }
     } else if (level === changeArr.length) {
       validPath = true;
-      console.log("end instruction", instruction);
     }
   } else {
     const arrayIdx = slicedArr.filter((el) => !isNaN(el));
-    console.log("arrayIdx: ", arrayIdx);
     for (let idx of arrayIdx) {
       let i = 0;
       const nbrIdx = arrayIdx ? slicedArr.indexOf(arrayIdx[i]) : null;
-      console.log("nbrIdx: ", nbrIdx);
       const newStr = slicedArr[nbrIdx - 1] + `[${idx}]`;
       slicedArr[nbrIdx - 1] = newStr;
-      console.log("newStr: ", newStr);
       slicedArr.splice(nbrIdx, 1);
-      console.log("slicedArr: ", slicedArr);
       i++;
     }
     errorMessage = `The property ${slicedArr.join(".")} is undefined`;
-    console.log("errorMessage: ", errorMessage);
   }
 
-  console.log("validPath: ", validPath);
   if (validPath) {
     return validPath;
   } else {
@@ -73,71 +58,77 @@ const pathController = (config, changeArr, level) => {
   }
 };
 
+const errorConsoleOutput = (errors, changesEntries) => {
+  const errorObj = { ...errors };
+  const errorEntries = Object.entries(errorObj);
+
+  console.dir("######################### ERROR LOG #########################");
+  for (let error of errorEntries) {
+    const transfNbr = error[0].match(/\d+/gm)[0];
+    const errorOutput = `- Transformation n°${transfNbr}: "${
+      changesEntries[transfNbr - 1][0]
+    }" failed / Cause: ${error[1]}`;
+    console.dir(errorOutput);
+  }
+  console.dir("---------------------- END ---------------------");
+};
+
 const instructionController = (configurationFile, changesList) => {
   const configuration = { ...configurationFile };
   const changes = { ...changesList };
-
   const configurationDepth = objectDepth(configuration);
-
   const changesEntries = Object.entries(changes);
-
   const errorObj = {};
-
   let changeRank = 1;
 
   for (let change of changesEntries) {
-    // console.log("changeRank: ", changeRank);
     let errorMessage = "";
     const pathArr = change[0].split(".");
     const regexp = /.+\[\d+\]$/gm;
-    const result = pathArr.filter((word) => regexp.test(word));
-    const indexResult = pathArr.indexOf(result[0]);
-    const arrayIdx = result[0].match(/\[\d+\]/gm);
-    // pathArr[indexResult].replace(/\[\d+\]/gm, "");
-    // console.log(arrayIdx[0].length);
-    pathArr[indexResult] = pathArr[indexResult].substring(
-      0,
-      pathArr[indexResult].length - arrayIdx[0].length
-    );
-    // console.log(pathArr[indexResult]);
-    pathArr.splice(indexResult + 1, 0, arrayIdx[0][1]);
-    console.log(pathArr);
+    const results = pathArr.filter((word) => regexp.test(word));
+    if (results) {
+      for (let result of results) {
+        const indexResult = pathArr.indexOf(result);
+        const arrayIdx = result.match(/\[\d+\]/gm);
+        pathArr[indexResult] = pathArr[indexResult].substring(
+          0,
+          pathArr[indexResult].length - arrayIdx[0].length
+        );
+        pathArr.splice(indexResult + 1, 0, arrayIdx[0][1]);
+      }
+    }
 
     if (pathArr.length > configurationDepth) {
-      errorMessage = "this transformation exceeds the config file's depth";
+      errorMessage = "This transformation exceeds the config file's depth";
     } else {
       const pathControlResult = pathController(
         configuration,
         pathArr,
         (level = 1)
       );
-      // console.log("doesPathExist: ", doesPathExist);
 
       if (pathControlResult === true) {
         const arrLastElement = pathArr.pop();
         let instructionPath = pathArr.reduce((prev, curr) => {
           return prev[curr];
         }, configuration);
-        console.log("instruction path: ", instructionPath);
-        console.log(instructionPath);
         const newValueType = typeof change[1];
-        if (newValueType === "object" && Array.isArray(change[1]))
-          change[1] = `[${change[1]}]`;
-        if (newValueType === "object" && !Array.isArray(change[1]))
-          change[1] = `${JSON.stringify(change[1])}`;
-        if (newValueType === "string") change[1] = `${change[1]}`;
+        if (newValueType === "object") {
+          if (objectDepth(change[1]) > 1) {
+            errorMessage = "The new value shouldn't exceed 1 line";
+          }
+          if (Array.isArray(change[1])) {
+            change[1] = `[${change[1]}]`;
+          } else {
+            change[1] = `${JSON.stringify(change[1])}`;
+          }
+        } else {
+          change[1] = `"${change[1]}"`;
+        }
 
         const update = JSON.parse(`{ "${arrLastElement}": ${change[1]} }`);
-        console.log("update: ", update);
-        // console.log(typeof update);
         Object.assign(instructionPath, update);
-        console.log(JSON.stringify(configuration));
-        // configuration.page1.initialSettings.color = change[1];
-        // console.log(
-        //   configuration.page1.initialSettings.color === instructionPath
-        // );
       } else {
-        console.log(pathControlResult);
         errorMessage = pathControlResult;
       }
     }
@@ -149,11 +140,11 @@ const instructionController = (configurationFile, changesList) => {
     }
     errorMessage = "";
     changeRank++;
-    // console.log("changeRank bottom: ", changeRank);
   }
-  console.log(errorObj);
-
+  errorConsoleOutput(errorObj, changesEntries);
   return configuration;
 };
 
-instructionController(configurationFile, changesList);
+console.log(
+  JSON.stringify(instructionController(configurationFile, changesList))
+);
